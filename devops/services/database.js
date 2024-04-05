a4b4260c21c1abee8b54ffa33d0a64ee58d7ad8f
MaxiveLegend <m.bongers@student.avans.nl>
2024-04-05 14:36:13 +0200

    removed unneeded enter

--- devops/services/database.js
+++ devops/services/database.js
@@ -3,7 +3,6 @@ const uri = process.env.MONGO_URL;
 const client = new MongoClient(uri);
 const db = client.db(process.env.DB_NAME || "mydb");
 
-
 module.exports = {
     db: db,
     client: client
